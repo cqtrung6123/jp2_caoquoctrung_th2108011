0781053a44cac59becc14119d057609f53636481
cqtrung6123 <93904342+cqtrung6123@users.noreply.github.com>
2022-04-27 15:36:49 +0700

    baikiemtra_jp2

---
+++ jp2_caoquoctrung__th2108011/src/Manager.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Manager {
+    List<Customer>customerList=new ArrayList<Customer>();
+
+
+    public Manager(){
+        this.customerList=new ArrayList<Customer>();
+    }
+    public Manager(List<Customer> customerList) {
+        this.customerList = customerList;
+    }
+
+
+    public void addCustomer(int id,String name,String email,String phone){
+        Customer customer=new Customer();
+        customer.setId(id);
+        customer.setName(name);
+        customer.setEmail(email);
+        customer.setPhone(phone);
+        customerList.add(customer);
+    }
+    public void findByName(String name){
+        boolean check=false;
+        for (int i=0;i<customerList.size();i++){
+            if(customerList.get(i).getName().contains(name)){
+                System.out.println(customerList.get(i));
+                check=true;
+            }
+        }
+        if (!check){
+            System.out.println("Not found");
+        }
+    }
+    public void displayAll(){
+        for (Customer customer:customerList) {
+            System.out.println(customer);
+        }
+    }
+}
